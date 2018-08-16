42c97585ea016e3636efdb594d5b47e51f91e68c
Unknown <Gortega713@west-mec.org>
2018-08-15 18:25:53 -0700

    Added JS file and started code
    Started scaffolding out code

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+      Project 01_06_01
+
+      Author: Gabriel Ortega
+      Date: 8.15.16  
+
+      Filename: script.js
+*/
+
+"use strict"
+
+var formValidity = true;
+
+// Function to validate form on submit
+
+function validateForm() {
+    
+}
+
+// Function to create all event listeners
+
+function createEventListeners() {
+    alert("function is running");
+}
+
+// Event listener for when the page loads
+ window.addEventListener("load", createEventListeners);
